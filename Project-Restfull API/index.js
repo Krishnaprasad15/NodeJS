26cf3e56e40310da4516ae76410eb312cb7c0021
krishna1500 <krishna16151@gmail.com>
2024-02-14 23:47:58 +0530

    Connecting to mongo

--- Project-Restfull API/index.js
+++ Project-Restfull API/index.js
@@ -1,82 +1,83 @@
-const express = require('express');
+const express= require('express');
+const mongoose = require('mongoose');
+
 const app = express();
-const mongoose= require('mongoose');
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({extended:false}));
 
-// Connecting to mongoDB
-mongoose.connect('mongodb://127.0.0.1:27017/project-1')
-.then(()=> console.log('MongoDB connected!'))
-.catch((err)=> console.log('Mongo Error',err));
+mongoose.connect('mongodb://127.0.0.1:27017/myProject-1')
+.then(()=>console.log('MongoDB Connected!'))
+.catch((err)=> console.log('MongoDb error',err));
 
-// Creating a mongoDB schema
 const userSchema= new mongoose.Schema({
-    firstName:{
-        type:String,
-        required:true,
+    firstName: {
+        type: String,
+        required: true,
     },
-    lastName:{
-        type:String,
+    lastName: {
+        type: String,
     },
     email:{
         type:String,
         required:true,
-        unique:true,
+        unique: true,
     },
     jobTitle:{
-        type:String,
+        type: String,
     },
     gender:{
         type:String,
-    },
-},{timestamps:true})
+    }
 
-// Creating model with the above schema
+},{timestamps: true});
+
 const User= mongoose.model('user',userSchema);
+
 app.get('/',(req,res)=>{
-    res.send('Hello world');
+    res.send('HELLO FROM HOMEPAGE');
 })
 
 app.get('/users',async (req,res)=>{
-    const db= await User.find({});
+    const db= await User.find({})
     const html= `
         <ul>
-            ${db.map((user)=> `<li>${user.firstName} - ${user.email}</li>`).join('')}        
+            ${db.map((user)=> `<li>${user.firstName} - ${user.email}</li>`).join('')}
         </ul>
     `
     res.send(html);
 })
 
-app.get('/api/users',async (req,res)=>{
-    const db= await User.find({});
-    res.json(db);
+app.get('/api/users', async (req,res)=>{
+    const db = await User.find({});
+    res.json(db); 
 })
 
-app.post('/api/users',async (req,res)=>{
+app.post('/api/users', async (req,res)=>{
     const body = req.body;
-    const data = await User.create({
-        firstName:body.firstName,
-        lastName: body.lastName,
+
+    await User.create({
+        firstName : body.fname,
+        lastName: body.lname,
         email: body.email,
-        jobTitle: body.jobTitle,
+        jobTitle: body.job,
         gender: body.gender,
     })
-    // console.log(data);
-    res.status(201).json({status:'Success'});
+
+    res.status(201).json({status:'Created'});
 })
 
 app.route('/api/users/:id')
-.get(async(req,res)=>{
-    const id= await User.findById(req.params.id);
+.get(async (req,res)=>{
+    const id = await User.findById(req.params.id);
     res.json(id);
 })
-.patch(async (req,res)=>{
-    await User.findByIdAndUpdate(req.params.id,{lastName:'Kittu'});
-    res.json({status:'Success'});
+.patch( async (req,res)=>{
+    await User.findByIdAndUpdate(req.params.id,{lastName: 'Chikky'})
+    res.json({status:'SUCCESS'});
 })
 .delete(async (req,res)=>{
-    const id= await User.findByIdAndDelete(req.params.id);
-    console.log(id);
-    res.json({status:"Success"});
+    await User.findByIdAndDelete(req.params.id);
+    res.json({status:"SUCCESSFULLY DELETED!"})
 })
-app.listen('5000',()=> console.log("Server Started!"));
+
+app.listen(5000,()=> console.log('Server Started!'));
